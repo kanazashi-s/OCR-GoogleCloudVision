885309b9e83a30d98428025db1f586d848183fc5
zashio <kana@zashio.net>
2019-01-25 11:40:44 +0900

    updated programs

---
+++ detect_handwritten.py
@@ -0,0 +1,76 @@
+#Copyright [zashio]
+
+#Licensed under the Apache License, Version 2.0 (the "License");
+#you may not use this file except in compliance with the License.
+#You may obtain a copy of the License at
+
+#    http://www.apache.org/licenses/LICENSE-2.0
+
+#Unless required by applicable law or agreed to in writing, software
+#distributed under the License is distributed on an "AS IS" BASIS,
+#WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#See the License for the specific language governing permissions and
+#limitations under the License.
+
+
+import os
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"]= "***/***/***.json"
+
+import io
+import argparse
+
+
+def detect_handwritten_ocr(path):
+    """Detects handwritten characters in a local image.
+
+    Args:
+    path: The path to the local file.
+    """
+    from google.cloud import vision_v1p3beta1 as vision
+    client = vision.ImageAnnotatorClient()
+
+    with io.open(path, 'rb') as image_file:
+        content = image_file.read()
+
+    image = vision.types.Image(content=content)
+
+    # Language hint codes for handwritten OCR:
+    # en-t-i0-handwrit, mul-Latn-t-i0-handwrit
+    # Note: Use only one language hint code per request for handwritten OCR.
+#    image_context = vision.types.ImageContext(
+#        language_hints=['ja-t-i0-handwrit'])
+
+    response = client.document_text_detection(image=image,
+#                                              image_context=image_context
+                                             )
+
+    print('Full Text: {}'.format(response.full_text_annotation.text))
+    for page in response.full_text_annotation.pages:
+        for block in page.blocks:
+            print('\nBlock confidence: {}\n'.format(block.confidence))
+
+            for paragraph in block.paragraphs:
+                print('Paragraph confidence: {}'.format(
+                    paragraph.confidence))
+
+                for word in paragraph.words:
+                    word_text = ''.join([
+                        symbol.text for symbol in word.symbols
+                    ])
+                    print('Word text: {} (confidence: {})'.format(
+                        word_text, word.confidence))
+
+#                    for symbol in word.symbols:
+#                        print('\tSymbol: {} (confidence: {})'.format(
+#                            symbol.text, symbol.confidence))
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description=__doc__,
+        formatter_class=argparse.RawDescriptionHelpFormatter)
+
+    parser.add_argument('filename')
+
+    args = parser.parse_args()
+
+    detect_handwritten_ocr(args.filename)
